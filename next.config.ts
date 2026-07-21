2f0245f63f5b74e48067b55f2f8e85b23d067380
Ariprakash <ariprakashapk@gmail.com>
2026-07-21 11:58:55 +0530

    feat: Configure GitHub Pages deployment with GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repo = 'Baliga-Website'; // Github Repository Name
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enables static export (generates an 'out' folder)
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js default image optimization engine
+  },
+  basePath: isGithubActions ? `/${repo}` : '',
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
 };
 
 export default nextConfig;
